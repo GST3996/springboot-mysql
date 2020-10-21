47b29ca1b57810d77da638284fcd87ee513b1dca
saravana1512 <saravanakumarsa@hcl.com>
2020-10-22 01:48:36 +0530

    first commit

---
+++ src/main/java/com/springboot/mysql/empdep/SpringbootApplication.java
@@ -0,0 +1,15 @@
+package com.springboot.mysql.empdep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"com.springboot.mysql.empdep.ctrl" ,"com.springboot.mysql.empdep.entity","com.springboot.mysql.empdep.repository","com.springboot.mysql.empdep.service"})
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/springboot/mysql/empdep/ctrl/Mycontroller.java
@@ -0,0 +1,12 @@
+package com.springboot.mysql.empdep.ctrl;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Mycontroller {
+	
+	
+
+}
